3701337aad4dce983e0780ef24534cd31087058c
OldinPadawan <diyandmakers@gmail.com>
2022-11-13 12:32:56 +0300

    added files to remote repository

---
+++ Lab_Two/Lab_Two/PeopleContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Two
+{
+    class PeopleContext : DbContext
+    {
+        public PeopleContext() : base("MyConnectionStrings")
+        {
+
+        }
+        public DbSet<Person> Persons {get; set;}
+    }
+}
